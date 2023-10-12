f480e6548d7286e7d11d12eb82846c981b23450b
Daniel Komés <danielkomes@gmail.com>
2023-10-12 19:06:57 -0300

    Ejercicio 1

--- PAC.Tests.WebApi/StudentControllerTest.cs
+++ PAC.Tests.WebApi/StudentControllerTest.cs
@@ -9,10 +9,44 @@ using PAC.WebAPI;
 using Microsoft.AspNetCore.Mvc;
 
 [TestClass]
-public class StudentControllerTest
+public class UsuarioControllerTest
 {
-        [TestInitialize]
-        public void InitTest()
-        {
-        }
+    private Mock<IStudentLogic>? mock;
+    private StudentController? controller;
+    private Student? student;
+
+    [TestInitialize]
+    public void InitTest()
+    {
+        mock = new Mock<IStudentLogic>(MockBehavior.Strict);
+        controller = new StudentController(mock.Object);
+    }
+
+    [TestMethod]
+    public void PostStudentOk()
+    {
+        student = new Student();
+        mock.Setup(m => m.InsertStudents(It.IsAny<Student>()));
+
+        var result = controller!.Post(student!);
+        var objectResult = result as OkResult;
+        var statusCode = objectResult!.StatusCode;
+
+
+        mock!.VerifyAll();
+        Assert.AreEqual(200, statusCode);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(Exception))]
+    public void PostStudentFail()
+    {
+        mock!.Setup(x => x.InsertStudents(It.IsAny<Student>())).Throws(new Exception());
+        var result = controller!.Post(It.IsAny<Student>());
+        var objectResult = result as ObjectResult;
+        var statusCode = objectResult!.StatusCode;
+
+        mock.VerifyAll();
+        Assert.AreEqual(500, statusCode);
+    }
 }
--- PAC.WebAPI/Controllers/StudentController.cs
+++ PAC.WebAPI/Controllers/StudentController.cs
@@ -14,5 +14,19 @@ namespace PAC.WebAPI
     [Route("[controller]")]
     public class StudentController : ControllerBase
     {
+        private readonly IStudentLogic _studentLogic;
+
+        public StudentController(IStudentLogic studentLogic)
+        {
+            this._studentLogic = studentLogic;
+        }
+
+        [AuthorizationFilter]
+        [HttpPost]
+        public IActionResult Post([FromBody] Student value)
+        {
+            _studentLogic.InsertStudents(value);
+            return Ok();
+        }
     }
 }
