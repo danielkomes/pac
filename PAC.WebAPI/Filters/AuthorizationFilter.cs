8b56f9f919ab11d1178bd70d3bf45742bee8701b
Daniel Komés <danielkomes@gmail.com>
2023-10-12 19:20:17 -0300

    Ejercicio 5

--- PAC.WebAPI/Filters/AuthorizationFilter.cs
+++ PAC.WebAPI/Filters/AuthorizationFilter.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace PAC.WebAPI.Filters
@@ -8,7 +7,16 @@ namespace PAC.WebAPI.Filters
     {
         public virtual void OnAuthorization(AuthorizationFilterContext context)
         {
-            var authorizationHeader = context.HttpContext.Request.Headers[""].ToString();
+            var authorizationHeader = context.HttpContext.Request.Headers["Authorization"].ToString();
+
+            Guid token = Guid.Empty;
+            if (string.IsNullOrEmpty(authorizationHeader) || !Guid.TryParse(authorizationHeader, out token))
+            {
+                context.Result = new ObjectResult(new { Message = "No puedo authorizarme" })
+                {
+                    StatusCode = StatusCodes.Status401Unauthorized
+                };
+            }
         }
 
     }
